d5da06d9a52fa9e720c8e67987cf7392c3734de3
frknue <furkan.uelker@hotmail.com>
2023-05-15 20:32:10 +0200

    import contentful content-model

--- src/lib/caisy/exportData/exportCaisyData.ts
+++ src/lib/caisy/exportData/exportCaisyData.ts
@@ -6,12 +6,11 @@ import { BlueprintUpsertInputInput } from "../types";
 export async function exportCaisyData(
   accessToken: string,
   projectId: string,
+  userId: string,
   inputPath: string
 ): Promise<void> {
-  // accessToken = "mBDi4i91ITJVflPppVudSFp2DLNBgqgI";
-  // projectId = "48f4bfbf-46b2-4c13-aa55-478dc8e1b1a0";
-  // inputPath = "./input";
-  const userId = "8ecf8def-af66-4897-982e-263d5f107be4";
+
+  userId = "8ecf8def-af66-4897-982e-263d5f107be4";
 
   const sdk = initSdk({ token: accessToken });
 
@@ -25,7 +24,7 @@ export async function exportCaisyData(
         userId: userId,
       },
     });
-    console.log("✅ Inserted blueprints:", insertedBlueprints);
+    // console.log("✅ Inserted blueprints:", insertedBlueprints);
   } catch (error) {
     console.error("❌ Error inserting blueprints:", error);
   }
--- src/lib/caisy/importData/saveToJson.ts
+++ src/lib/caisy/importData/saveToJson.ts
@@ -2,6 +2,7 @@ import fs from "fs";
 
 export async function saveToJson(
   data: any,
+  provider: string,
   projectId: string,
   contentType: string,
   outputPath: string
@@ -9,13 +10,15 @@ export async function saveToJson(
   data.forEach((element: any, index: number) => {
     const nameOrTitle = element.node.name || element.node.title;
     const title: string = nameOrTitle.replace(/[^a-zA-Z0-9]/g, "_");
-    const projectPath = `${outputPath}/${projectId}/${contentType}`;
-    //Todo name instead of title
+    const projectPath = `${outputPath}/${provider}/${projectId}/${contentType}`;
+
     const filePath = `${projectPath}/${title}.json`;
 
     if (!fs.existsSync(projectPath)) {
       fs.mkdirSync(projectPath, { recursive: true });
     }
+
     fs.writeFileSync(filePath, JSON.stringify(element.node, null, 2));
+    console.log(`✅ ${filePath}`);
   });
 }
