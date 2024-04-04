853351e846eb175c788309c282f0abf295af92af
gkoutzioulis <d.gkoutzioulis@cyclus.digital>
2024-04-04 18:18:57 +0300

    add download asset functionality

---
+++ src/lib/caisy/asset-files/asset.ts
@@ -0,0 +1,53 @@
+import { assetUrls } from "../../common/writer/content-entry";
+import { CaisyRunOptions } from "../provider";
+import fetch from "node-fetch";
+import { promises as fs } from "fs";
+import { v4 as uuidv4 } from "uuid";
+import path from "path";
+
+const RELATIVE_TARGET_DIR = "../../../../cport_assets";
+
+const downloadImage = async (url: string) => {
+  const response = await fetch(url);
+  if (!response.ok) throw new Error(`Failed to fetch image: ${response.statusText}`);
+  const buffer = await response.buffer();
+
+  const uuid = uuidv4();
+  const originalName = path.basename(url);
+  const newName = `${uuid}${originalName}`;
+  const targetDir = path.resolve(__dirname, RELATIVE_TARGET_DIR);
+  const filePath = path.join(targetDir, newName);
+
+  await fs.mkdir(targetDir, { recursive: true });
+
+  await fs.writeFile(filePath, buffer);
+  console.log(`Image saved as ${newName}`);
+};
+
+export const assetFiles = async ({
+  sdk,
+  projectId,
+  after,
+  onProgress,
+  onError,
+}: CaisyRunOptions & { after: string | null }) => {
+  const downloadPromises = Array.from(assetUrls).map(async (url) => {
+    const modifiedUrl = `${url}?original`;
+    try {
+      await downloadImage(modifiedUrl);
+    } catch (error) {
+      console.error(`Failed to download image from ${url}:`, error);
+    }
+  });
+  await Promise.all(downloadPromises);
+};
+
+export const exportCaisyAssets = async ({ sdk, projectId, onError, onProgress }: CaisyRunOptions): Promise<void> => {
+  await assetFiles({
+    sdk,
+    projectId,
+    onError,
+    onProgress,
+    after: null,
+  });
+};
